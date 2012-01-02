4015b41b95732cae78a3946affa40a3634558c96
Peter Graham <peterghm@gmail.com>
2012-01-02 12:13:23 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PicMixr::Application.config.secret_token = 'fad93976286edc7ece150a01b07bcfbcb75d6354956b9c82b7cda953a7f837125332b8a51eedd544d0ba705770725ccc56da9f262ee3d5ea01d51378aea225a1'
