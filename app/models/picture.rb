db1e76f6ee3be5efff83941ec0cca1401e15117a
Peter Graham <peterghm@gmail.com>
2012-01-17 22:10:33 -0500

    S3 URL should use HTTPS

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,6 +3,7 @@ class Picture < ActiveRecord::Base
     :storage => :s3,
     :path => ":id-:style.:extension",
     :bucket => ENV['S3_BUCKET'],
+    :s3_protocol => 'https',
     :s3_credentials => {
       :access_key_id => ENV['S3_KEY'],
       :secret_access_key => ENV['S3_SECRET']
