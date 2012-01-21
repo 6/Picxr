9f8892b6598fb74adf6a423cc367cda917bd439d
Peter Graham <peterghm@gmail.com>
2012-01-21 10:00:52 -0500

    make it easier to show debug messages on production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :set_is_dev
   helper_method :current_user
   
   private
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  def set_is_dev
+    @is_dev = Rails.env.development? || !params[:dev].nil? ? 'yes' : 'no'
   end
   
 end
