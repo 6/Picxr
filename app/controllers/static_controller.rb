ed1920fed2b20ca1062f93e3986f2c83a6863e6e
Peter Graham <peterghm@gmail.com>
2012-01-27 16:03:30 -0500

    handle OG:URL metadata correctly

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,2 +1,9 @@
 class StaticController < ApplicationController
+  before_filter :generate_permalink
+  
+  private
+  
+  def generate_permalink
+    @permalink = "http://#{ENV['PERMALINK_ROOT']}#{request.path}"
+  end
 end
